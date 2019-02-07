85bda7cfd058d0abe186e385833790337b2e6498
Jess <shaderone.jd@gmail.com>
2019-02-06 19:14:43 -0800

    3.5 hour mark: matches retrieved, passed to client

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,91 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
+
+import Match from './components/Match.js'
 
 export default class App extends Component {
-  state = { username: null };
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      region: 'NA',
+      matches: [],
+    };
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    this.handleUserInput = this.handleUserInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUserInput(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(`Your summoner name is: ${this.state.name}`);
+    console.log(`Your region is: ${this.state.region}`);
+    event.preventDefault();
+
+    const userData = {
+      name: this.state.name,
+      region: this.state.region
+    };
+
+    fetch('/api/search-summoner', {
+      method: 'post',
+      body: JSON.stringify(userData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json()).then((data) => {
+      this.setState({
+        matches: data.matches
+      });
+    });
   }
 
   render() {
-    const { username } = this.state;
+    const { name, region, matches } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        <h1>Hello Summoner</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="name" placeholder="Enter summoner name here" value={name} onChange={this.handleUserInput} />
+          <select name="region" value={region} onChange={this.handleUserInput}>
+            <option value="BR1">BR</option>
+            <option value="EUN1">EUNE</option>
+            <option value="EUW1">EUW</option>
+            <option value="JP1">JP</option>
+            <option value="KR">KR</option>
+            <option value="LA1">LAN</option>
+            <option value="LA2">LAS</option>
+            <option value="NA1">NA</option>
+            <option value="OC1">OCE</option>
+            <option value="T1">TR</option>
+            <option value="RU">RU</option>
+            <option value="PBE1">PBE</option>
+          </select>
+          <input type="submit" value="Search" />
+        </form>
+        <div className="results-container">
+          <Matches matches={matches} />
+        </div>
       </div>
     );
   }
 }
+
+const Matches = ({ matches }) => {
+  console.log(matches);
+  if (matches.length) {
+    return (
+      <ul>
+        {matches.map(match => <Match match={match} />)}
+      </ul>
+    );
+  }
+
+  return null;
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
-const os = require('os');
 
 const app = express();
+process.env.LEAGUE_API_KEY = 'RGAPI-33195678-8070-440a-8cbd-14c72e9b2a0e';
+
+const LeagueJS = require('leaguejs');
+const leagueJs = new LeagueJS(process.env.LEAGUE_API_KEY, {
+  useV4: true
+});
+
+app.use(require('body-parser').json());
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/', (req, res) => res.send('HELLO'));
+app.post('/api/search-summoner', (req, res) => {
+  const { name, region } = req.body;
+
+  leagueJs.Summoner
+    .gettingByName(name, region)
+    .then((account) => {
+      leagueJs.Match
+        .gettingListByAccount(account.accountId, region, {
+          endIndex: 5,
+        })
+        .then((matchData) => {
+          res.send(matchData);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send('error');
+    });
+});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
